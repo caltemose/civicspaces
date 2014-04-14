59ee997e001c2cd188e42d0346f80ee3544d326e
Chad Altemose <chad@chadzilla.com>
2014-04-14 11:22:49 -0400

    lost-password sequence in progress. last commit before mandrill integration.

--- routes/password.js
+++ routes/password.js
@@ -1,7 +1,22 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
 module.exports = function (app) {
 
   app.get('/password/lost', function (req, res) {
     res.render('password/lost.jade');
   });
+
+  app.post('/password/lost', function(req, res) {
+    var email = req.params('email');
+    if (!email) return res.render('password/lost.jade', {noEmail:true});
+    User.findById(email, function (err, user) {
+      if (err) return next(err);
+      if (!user) return res.render('password/lost.jade', {noUser:true});
+      else {
+        return res.render('password/lost-results.jade', {user:user});
+      }
+    })
+  })
 
 }
